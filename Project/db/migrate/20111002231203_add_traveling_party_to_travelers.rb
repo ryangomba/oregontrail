62d22814ecfbb3db921fa6337a292530e7fd7cb0
Ryan Gomba <ryan@ryangomba.com>
2011-10-05 13:09:17 -0400

    Fixed migrations

--- Project/db/migrate/20111002231203_add_traveling_party_to_travelers.rb
+++ Project/db/migrate/20111002231203_add_traveling_party_to_travelers.rb
@@ -1,5 +1,5 @@
 class AddTravelingPartyToTravelers < ActiveRecord::Migration
   def change
-    add_column :travelers, :traveling_party, :relations
+    add_column :travelers, :traveling_party_id, :relations
   end
 end
