fdf7ef1a9256bcc7673904f0961101a3e8796a4e
Ryan Gomba <ryan@ryangomba.com>
2011-10-23 16:17:35 -0400

    Fixed all the indents

--- Project/app/models/item.rb
+++ Project/app/models/item.rb
@@ -1,54 +1,54 @@
 class Item < ActiveRecord::Base
 
-  def types
-    return ["Food", "Clothing", "Ammo", "Ox", "Wheel", "Axle", "Tongue"]
-  end
-  
-  def value;  0; end
-  def weight; 0; end
+    def types
+        return ["Food", "Clothing", "Ammo", "Ox", "Wheel", "Axle", "Tongue"]
+    end
 
-  belongs_to :trader
+    def value;  0; end
+    def weight; 0; end
 
-  validates_presence_of :type
+    belongs_to :trader
+
+    validates_presence_of :type
 
 end
 
 class VariableItem < Item
-  
-  validates_presence_of :health
-  
+
+    validates_presence_of :health
+
 end
 
 # REGULAR ITEMS
 
 class Clothing < Item
-  def value;  10; end
-  def weight; 5; end
+    def value;  10; end
+    def weight; 5; end
 end
 
 class Ammo < Item
-  def value;  1; end
-  def weight; 1; end
+    def value;  1; end
+    def weight; 1; end
 end
 
 # VARIABLE ITEMS
 
 class Ox < VariableItem
-  def value;  50; end
-  def weight; 0; end
+    def value;  50; end
+    def weight; 0; end
 end
 
 class Wheel < VariableItem
-  def value;  25; end
-  def weight; 10; end
+    def value;  25; end
+    def weight; 10; end
 end
 
 class Axle < VariableItem
-  def value;  50; end
-  def weight; 15; end
+    def value;  50; end
+    def weight; 15; end
 end
 
 class Tongue < VariableItem
-  def value;  100; end
-  def weight; 20; end
+    def value;  100; end
+    def weight; 20; end
 end
--- Project/app/models/leader.rb
+++ Project/app/models/leader.rb
@@ -1,11 +1,11 @@
 class Leader < Traveler
-  
-   PROFESSIONS = {
-     "Farmer" => 400,
-     "Banker" => 1600,
-     "Carpenter" => 800,
-     }
-    
+
+    PROFESSIONS = {
+        "Farmer" => 400,
+        "Banker" => 1600,
+        "Carpenter" => 800,
+    }
+
     validates_presence_of :profession
     validates_inclusion_of :profession, :in => PROFESSIONS.keys
 
